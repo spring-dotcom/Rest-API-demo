01e6dc2603369b1945bf8483bd08bb987c38cea3
spring-dotcom <khushi.chauhan@hcltech.com>
2025-09-23 04:09:15 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
+
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello from Java REST API!";
+    public String sayHello() {
+        return "Hello, World!";
     }
 }
